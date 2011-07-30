16bf36a7044a468f693e5ca47fa319a61200bc2f
Gabriele Favalessa <gabriele@develer.com>
2011-07-30 10:39:53 +0200

    Cleaned up operands __repr__ and __str__

--- operands.py
+++ operands.py
@@ -15,79 +15,88 @@
 
 # -*- coding: utf-8 -*-
 
-class M_AC(object):
+class RegisterBase(object):
     def __init__(self, **kwargs):
         pass
 
-    def __eq__(self, other):
-        return isinstance(other, M_AC)
-
     def __repr__(self):
-        return "A"
+        return self.name
 
     def __str__(self):
         return ''
 
-class M_XR(object):
+    def __eq__(self, other):
+        return isinstance(other, RegisterBase) and self.name == other.name
+
+class M_AC(RegisterBase):
+    name = 'A'
+
+class M_XR(RegisterBase):
+    name = 'X'
+
+class M_YR(RegisterBase):
+    name = 'Y'
+
+class M_PC(RegisterBase):
+    name = 'PC'
+
+class M_SP(RegisterBase):
+    name = 'SP'
+
+class M_SR(RegisterBase):
+    name = 'SR'
+
+class FlagBase(object):
     def __init__(self, **kwargs):
         pass
 
     def __repr__(self):
+        return self.name
+
+    def __str__(self):
         return ''
 
-class M_YR(object):
-    def __init__(self, **kwargs):
-        pass
+class M_FC(FlagBase):
+    name = 'C'
 
-    def __repr__(self):
-        return ''
+class M_FD(FlagBase):
+    name = 'D'
 
-class M_AIND(object):
-    def __init__(self, **kwargs):
-        pass
+class M_FI(FlagBase):
+    name = 'I'
 
-class M_FC(object):
-    def __init__(self, **kwargs):
-        pass
-
-    def __repr__(self):
-        return ''
-
-class M_FD(object):
-    def __init__(self, **kwargs):
-        pass
-
-    def __repr__(self):
-        return ''
-
-class M_FI(object):
-    def __init__(self, **kwargs):
-        pass
-
-    def __repr__(self):
-        return ''
-
-class M_FV(object):
-    def __init__(self, **kwargs):
-        pass
-
-    def __repr__(self):
-        return ''
+class M_FV(FlagBase):
+    name = 'V'
 
 class M_IMM(object):
     def __init__(self, **kwargs):
         self.immed = kwargs['immed']
 
     def __repr__(self):
+        return str(self)
+
+    def __str__(self):
         return '#$%02X' % self.immed
 
 class M_INDX(object):
     def __init__(self, **kwargs):
         self.offset = kwargs['offset']
 
+    def __repr__(self):
+        return str(self)
+
+    def __str__(self):
+        return '($%02X, X)' % self.offset
+
 class M_INDY(object):
     def __init__(self, **kwargs):
         self.offset = kwargs['offset']
+
+    def __repr__(self):
+        return str(self)
+
+    def __str__(self):
+        return '($%02X),Y' % self.offset
 
 class M_NONE(object):
     def __init__(self, **kwargs):
@@ -97,14 +106,14 @@ class M_NONE(object):
         return isinstance(other, M_NONE)
 
     def __repr__(self):
+        return '_'
+
+    def __str__(self):
         return ''
 
-class M_PC(object):
+class M_AIND(object):
     def __init__(self, **kwargs):
         pass
-
-    def __repr__(self):
-        return ''
 
 class M_REL(object):
     def __init__(self, **kwargs):
@@ -120,23 +129,17 @@ class M_REL(object):
         addr += self.offset + 2
         return memory.addr_label(addr)
 
-class M_SP(object):
-    def __init__(self, **kwargs):
-        pass
-
-    def __repr__(self):
-        return ''
-
-class M_SR(object):
-    def __init__(self, **kwargs):
-        pass
-
 class AddrBase(object):
     def __init__(self, **kwargs):
         self.addr = kwargs['addr']
 
+    def __repr__(self):
+        fmt = '$%%0%dX' % self.size
+
+        return fmt % self.addr
+
     def to_string(self, addr, memory):
-        return memory.addr_label(self.addr)
+        return memory.addr_label(self.addr, size=self.size)
 
 class M_ABS(AddrBase):
     size = 4
